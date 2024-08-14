8334f11083f3fbb114f442b0cb9c240df72c0f85
Silovisk <silasaraujo1@hotmail.com>
2024-08-14 16:52:29 -0300

    feat: Add VacationPlanSeeder for seeding vacation plan data

---
+++ database/seeders/VacationPlanSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\VacationPlan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VacationPlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        VacationPlan::factory()->count(100)->create();
+    }
+}
