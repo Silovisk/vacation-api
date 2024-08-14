ae5344392f1c6eb31b18a3764a64b2579ad09936
Silovisk <silasaraujo1@hotmail.com>
2024-08-14 15:31:01 -0300

    refactor: Improve VacationPlanException handling and response formatting

--- app/Exceptions/VacationPlanException.php
+++ app/Exceptions/VacationPlanException.php
@@ -3,24 +3,46 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class VacationPlanException extends Exception
 {
-    /**
-     * Report the exception.
-     */
-    public function report(): void
+    protected $statusCode;
+
+    public function __construct(string $message, int $statusCode = 500)
     {
-        //
+        parent::__construct($message);
+        $this->statusCode = $statusCode;
     }
 
-    /**
-     * Render the exception as an HTTP response.
-     */
-    public function render(Request $request): Response
+    public function getStatusCode(): int
     {
-        //
+        return $this->statusCode;
+    }
+
+    public static function notFound(): self
+    {
+        return new self('Vacation plan not found.', 404);
+    }
+
+    public static function notCreated(): self
+    {
+        return new self('Vacation plan not created.', 400);
+    }
+
+    public static function notUpdated(): self
+    {
+        return new self('Vacation plan not updated.', 400);
+    }
+
+    public static function notDeleted(): self
+    {
+        return new self('Vacation plan not deleted.', 500);
+    }
+
+    public function render($request)
+    {
+        return response()->json([
+            'error' => $this->message,
+        ], $this->statusCode);
     }
 }
