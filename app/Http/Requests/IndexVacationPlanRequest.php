bd9959e1a7e263fb05bd9f31f8e8db296ee2d95f
Silovisk <silasaraujo1@hotmail.com>
2024-08-14 15:35:15 -0300

    feat: Enable pagination for vacation plans in index request

--- app/Http/Requests/IndexVacationPlanRequest.php
+++ app/Http/Requests/IndexVacationPlanRequest.php
@@ -11,7 +11,7 @@ class IndexVacationPlanRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class IndexVacationPlanRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'per_page' => 'integer|min:1|max:100',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'per_page.integer' => 'The per_page field must be an integer.',
+            'per_page.min' => 'The per_page field must be at least 1.',
+            'per_page.max' => 'The per_page field must not be greater than 100.',
         ];
     }
 }
