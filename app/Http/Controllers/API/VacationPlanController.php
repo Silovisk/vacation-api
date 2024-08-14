e0bb1a906af6a1d4f5a1ec797b75e7acba2dc029
Silovisk <silasaraujo1@hotmail.com>
2024-08-14 15:23:48 -0300

    refactor: Add VacationPlanController for managing vacation plans in the API

---
+++ app/Http/Controllers/API/VacationPlanController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Exceptions\VacationPlanException;
+use App\Http\Requests\IndexVacationPlanRequest;
+use App\Http\Resources\VacationPlanResource;
+use App\Models\VacationPlan;
+use App\Http\Requests\StoreVacationPlanRequest;
+use App\Http\Requests\UpdateVacationPlanRequest;
+use App\Services\VacationPlanService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+
+class VacationPlanController extends BaseController
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(IndexVacationPlanRequest $request)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVacationPlanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VacationPlan $vacationPlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVacationPlanRequest $request, VacationPlan $vacationPlan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VacationPlan $vacationPlan)
+    {
+        //
+    }
+}
