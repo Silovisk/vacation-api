2a0365190c04b57524dec94930945a36e396df4d
Silovisk <silasaraujo1@hotmail.com>
2024-08-16 16:29:37 -0300

    refactor: Update StoreVacationPlanRequest to include custom error response for validation errors

--- app/Http/Requests/StoreVacationPlanRequest.php
+++ app/Http/Requests/StoreVacationPlanRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreVacationPlanRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreVacationPlanRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,19 @@ class StoreVacationPlanRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'date' => 'required|date_format:Y-m-d',
+            'location' => 'required|string|max:255',
+            'participants' => 'array',
+            'participants.*' => 'string'
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'form-errors' => $validator->errors()
+        ], 422));
     }
 }
