7d4c49d890197eeeffd48fe9f23bf9a2e471bfcb
Silovisk <silasaraujo1@hotmail.com>
2024-08-18 18:51:07 -0300

    refactor: Update API routes to use AuthController instead of RegisterController

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\VacationPlanController;
 
-Route::controller(RegisterController::class)->group(function(){
+Route::controller(AuthController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
